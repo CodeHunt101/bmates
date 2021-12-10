cb3bfaa467139c527a894784aeee113a570c91cd
Harold Torres Marino <haroldtm55@gmail.com>
2021-12-10 20:50:06 +1100

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,72 +6,72 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# harold = User.create({
-#   first_name: "Harold",
-#   last_name: "Torres",
-#   gender: "M",
-#   bio: "blah blah blah",
-#   username: "haroldtm55",
-#   email: "harold@example.com",
-#   mate: true,
-#   password: "password",
-#   password_confirmation: "password"
-# })
+harold = User.create({
+  first_name: "Harold",
+  last_name: "Torres",
+  gender: "M",
+  bio: "blah blah blah",
+  username: "haroldtm55",
+  email: "harold@example.com",
+  mate: true,
+  password: "password",
+  password_confirmation: "password"
+})
 
-# harold_listing = Listing.create({
-#   title: "Platonic Sugar Daddy",
-#   description: "bleh bleh",
-#   mate: harold
-# })
+harold_listing = Listing.create({
+  title: "Platonic Sugar Daddy",
+  description: "bleh bleh",
+  mate: harold
+})
 
-# siri = User.create({
-#   first_name: "Siri",
-#   last_name: "Watasir",
-#   gender: "F",
-#   bio: "blah blah blah",
-#   username: "chongnang",
-#   email: "siri@example.com",
-#   mate: false,
-#   password: "password",
-#   password_confirmation: "password"
-# })
+siri = User.create({
+  first_name: "Siri",
+  last_name: "Watasir",
+  gender: "F",
+  bio: "blah blah blah",
+  username: "chongnang",
+  email: "siri@example.com",
+  mate: false,
+  password: "password",
+  password_confirmation: "password"
+})
 
-# mate = [true, false]
-# gender = ['M', 'F']
+mate = [true, false]
+gender = ['M', 'F']
 
-# i=0
-# while i<100 do
-#   User.create(
-#     first_name: i<=50 ? Faker::Name.male_first_name : Faker::Name.female_first_name,
-#     last_name: Faker::Name.last_name,
-#     gender: i <= 50 ? 'M' : 'F',
-#     bio: "blah blah blah",
-#     username: "user" + "#{i+1}",
-#     email: Faker::Internet.email,
-#     mate: mate[rand(0..1)],
-#     password: "password",
-#     password_confirmation: "password"
-#   )
-#   i+=1
-# end
+i=0
+while i<100 do
+  User.create(
+    first_name: i<=50 ? Faker::Name.male_first_name : Faker::Name.female_first_name,
+    last_name: Faker::Name.last_name,
+    gender: i <= 50 ? 'M' : 'F',
+    bio: "blah blah blah",
+    username: "user" + "#{i+1}",
+    email: Faker::Internet.email,
+    mate: mate[rand(0..1)],
+    password: "password",
+    password_confirmation: "password"
+  )
+  i+=1
+end
 
-# siri_reservation = Reservation.create({
-#   listing: harold_listing,
-#   member: siri,
-#   status: "pending",
-#   checkin: '2022-02-02 01:00:00',
-#   checkout: '2022-02-02 02:00:00' 
-# })
+siri_reservation = Reservation.create({
+  listing: harold_listing,
+  member: siri,
+  status: "pending",
+  checkin: '2022-02-02 01:00:00',
+  checkout: '2022-02-02 02:00:00' 
+})
 
-# mates = User.select{|u| u.mate}
+mates = User.select{|u| u.mate}
 
-# mates.each do |mate|
-#   Listing.create({
-#     title: Faker::Quotes::Chiquito.sentence,
-#     description: "bleh bleh",
-#     mate: mate
-#   })
-# end
+mates.each do |mate|
+  Listing.create({
+    title: Faker::Quotes::Chiquito.sentence,
+    description: "bleh bleh",
+    mate: mate
+  })
+end
 
 members = User.select{|u| !u.mate}
 
